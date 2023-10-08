00d438ccfcdba1a159a0bc49d85e9faf2a3defb4
yejin <yj040102@naver.com>
2023-10-08 20:05:04 +0900

    week3

---
+++ 솝/week3/src/App.js
@@ -0,0 +1,16 @@
+import MovieDetail from "../src/Components/MovieDetail.jsx";
+import Movie from "../src/Components/Movie.jsx";
+
+function App() {
+  return (
+
+    <div className="Movie-container">
+            <Movie/>
+    </div>
+
+
+  );
+}
+
+export default App;
+
---
+++ 솝/week3/src/Components/MovieDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { movies } from '../movieDummy';
+import '../index.css';
+
+function MovieDetail() {
+  return (
+    <div className='detail-container'>
+      {movies.results.map((movie) => (
+        <div key={movie.id} className='detail-one' >
+         
+          <p className='title_detail'>{movie.title}</p>
+          <p className='overview'>{movie.overview}</p>
+          
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MovieDetail;
