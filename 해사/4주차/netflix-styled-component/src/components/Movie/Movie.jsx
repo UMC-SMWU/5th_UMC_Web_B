948f070cd686b18276127d74d7caca73c0e316d6
haesa <xdhaesa@gmail.com>
2023-10-16 06:24:29 +0900

    [Chore]: eslint & prettier 설정

--- 해사/4주차/netflix-styled-component/src/components/Movie/Movie.jsx
+++ 해사/4주차/netflix-styled-component/src/components/Movie/Movie.jsx
@@ -2,18 +2,23 @@ import MovieDetail from '../MovieDetail/MovieDetail';
 import { MovieItem, Poster, Meta, Title } from './Movie.style';
 
 export default function Movie({
-  movie: { poster_path, title, vote_average, overview },
+  movie: {
+    poster_path: posterPath,
+    title,
+    vote_average: voteAverage,
+    overview,
+  },
 }) {
   return (
     <MovieItem>
       <MovieDetail title={title} overview={overview} />
       <Poster
-        src={`https://image.tmdb.org/t/p/w1280/${poster_path}`}
+        src={`https://image.tmdb.org/t/p/w1280/${posterPath}`}
         alt={title}
       />
       <Meta>
         <Title>{title}</Title>
-        <span>{vote_average}</span>
+        <span>{voteAverage}</span>
       </Meta>
     </MovieItem>
   );
