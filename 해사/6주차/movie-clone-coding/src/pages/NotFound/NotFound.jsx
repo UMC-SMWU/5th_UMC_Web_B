afc32823c31c17765638e5086ce1683a68203f60
haesa <xdhaesa@gmail.com>
2023-11-09 21:34:02 +0900

    [Feat]: 6주차 미션 구현

--- 해사/6주차/movie-clone-coding/src/pages/NotFound/NotFound.jsx
+++ 해사/6주차/movie-clone-coding/src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './NotFound.module.css';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+  const handleClick = () => navigate('/');
+
   return (
     <div className={styles.page}>
       <h1>해당 페이지를 찾지 못했습니다.</h1>
       <p>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다.</p>
-      <Link to='/' className={styles.toMain}>
+      <p className={styles.toMain} onClick={handleClick}>
         메인 페이지로 이동
-      </Link>
+      </p>
     </div>
   );
 }
