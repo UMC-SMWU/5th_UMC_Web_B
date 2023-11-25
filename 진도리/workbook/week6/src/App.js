a2cd558efe457984c3f6cb8c8ad6dddaa266c6ba
jinju <hor629@naver.com>
2023-11-25 19:02:42 +0900

    [Feat(진도리/유진주)] 6주차

---
+++ 진도리/workbook/week6/src/App.js
@@ -0,0 +1,34 @@
+import "./index.css";
+
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Celebrity from "./pages/Celebrity";
+import TV from "./pages/TV";
+import NotFound from "./pages/NotFound";
+import MovieDetail from "./pages/MovieDetail";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="root-wrap">
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route exact path="/home" element={<Home/>} />
+            <Route exact path="/movies" element={<Movies/>} />
+            <Route exact path="/celebrity" element={<Celebrity/>} />
+            <Route exact path="/tv" element={<TV/>} />
+            <Route exact path="/*" element={<NotFound/>} />
+            <Route path="/movies/:title" element={<MovieDetail/>}/>
+          </Routes>
+      </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
