195063ed0c7f89f3511ffe6bde11b9b20c3250d1
jinju <hor629@naver.com>
2023-11-26 17:26:56 +0900

    [Feat(진도리/유진주)] 8주차 미션]

---
+++ 진도리/workbook/week8/misson/client/src/redux/actions.js
@@ -0,0 +1,9 @@
+export const setUserId = (id) => ({
+    type: 'SET_USER_ID',
+    payload: id 
+    });
+
+export const setUserPassword = (password) => ({ 
+    type: 'SET_USER_PASSWORD',
+    payload: password
+});
